965bfcb4356e66d893eb3832e7531810765dfc2a
Whitesan <piotrekrek12@wp.pl>
2025-05-28 21:15:25 +0200

    added conf for ci cd

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          I was changed at 21:00
+          I was changed
         </a>
       </header>
     </div>
